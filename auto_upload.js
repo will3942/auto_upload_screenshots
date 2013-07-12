fc57e8e41f2e7dbc1b013542b90a23b831a13e2e
Will3942 <will@will3942.com>
2013-07-12 19:32:22 +0100

    Update auto_upload.js

--- auto_upload.js
+++ auto_upload.js
@@ -12,30 +12,30 @@ var web_location = '' // if screenshots are to be accessed online then this is t
 /* end editable variables */
 
 fs.watch(desktop_directory, function (event, filename) {
-  if (event == 'rename') {
-    if (filename.split('.').pop() == 'png') {
-    	var original = fs.createReadStream(desktop_directory + filename);
-    	var escaped = filename.replace(/\s/g, '-');
-    	var mainstream = fs.createWriteStream(uploaded_directory + escaped);
-    	original.pipe(mainstream);
-    	original.on('end', function() {
-    		/* fak ye */
-    		child = exec('scp ' + uploaded_directory + escaped + " " + ssh_command + ':' + remote_directory + escaped, function (error, stdout, stderr) {
-    			if (error == null) {
-    				exec('echo ' + web_location + escaped + ' | pbcopy', function (error, stdout, stderr) {
-    					if (error == null) {
-    						/* fak ye done */
-    					}
-    					else {
-    						console.log(error);
-    					}
-    				});
-    			}
-    			else {
-    				console.log(error);
-    			}
-			});
-    	});
+    if (event == 'rename') {
+        if (filename.split('.').pop() == 'png') {
+        	var original = fs.createReadStream(desktop_directory + filename);
+        	var escaped = filename.replace(/\s/g, '-');
+        	var mainstream = fs.createWriteStream(uploaded_directory + escaped);
+        	original.pipe(mainstream);
+        	original.on('end', function() {
+        		/* fak ye */
+        		child = exec('scp ' + uploaded_directory + escaped + " " + ssh_command + ':' + remote_directory + escaped, function (error, stdout, stderr) {
+        			if (error == null) {
+        				exec('echo ' + web_location + escaped + ' | pbcopy', function (error, stdout, stderr) {
+        					if (error == null) {
+        						/* fak ye done */
+        					}
+        					else {
+        						console.log(error);
+        					}
+        				});
+                    }
+                    else {
+                        console.log(error);
+                    }
+                });
+            });
+        }
     }
-  }
 });
